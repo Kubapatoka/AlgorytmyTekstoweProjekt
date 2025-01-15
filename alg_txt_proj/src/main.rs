f539acde2e22577915275d6088fa41552613761a
Kubapatoka <bukaj.k@wp.pl>
2025-01-15 12:35:51 +0100

    add suffix array

--- alg_txt_proj/src/main.rs
+++ alg_txt_proj/src/main.rs
@@ -1,16 +1,98 @@
-
-
-fn lz_77(text : &str)
-{
-    let mut vector_of_blocks: Vec<>
-
+struct Block {
+    start: u64,
+    end: u64,
+    letter: Char,
 }
 
+impl Block {
+    fn length() {
+        if start == 0 && end == 0 {
+            return 1;
+        }
+
+        return end - start + 1 + 1;
+    }
+}
+
+struct SuffixArray {
+    data_starts: Vec<u32>,
+    data_end: u32,
+    text: str,
+}
+
+impl SuffixArray {
+    fn add_letter() {
+        data_end += 1;
+        add(data_end);
+    }
+
+    fn is_greater(p1: u32, p2: u32) {
+        for i in range(0, min(data_end - p1, data_end - p2)) {
+            if text[p1 + i] > text[p2 + i] {
+                return true;
+            }
+            if text[p1 + i] < text[p2 + i] {
+                return false;
+            }
+        }
+
+        if p1 < p2 {
+            return false;
+        }
+        return true;
+    }
+
+    fn binary_search_new_pos(sp: u32) {
+        let mut start = 0;
+        let mut end = data_starts.len();
+
+        while start != end {
+            let mid = (start + end) / 2;
+
+            if is_greater(data_starts[mid], sp) {
+                end = mid;
+            } else {
+                start = mid;
+            }
+        }
+
+        return start;
+    }
+
+    fn add(start_pos: u32) {
+        let pos = data_starts.binary_search_new_pos(start_pos);
+        data_starts.insert(pos, start_pos)
+    }
+}
+
+struct LZ77 {
+    suff_arr: SuffixArray,
+    end_pos: u32,
+}
+
+impl LZ77 {
+    fn generate_block(text: &str, start: i32) -> Block {
+        
+    }
+
+    fn lz_77(text: &str) {
+        let mut vector_of_blocks: Vec<Block>;
+        let mut actual_end = 0;
+
+        let text_len = len(text);
+
+        while actual_end != text_len {
+            vector_of_blocks.push(generate_block(text, actual_end));
+
+            let block_len = vector_of_blocks.last().unwrap().length();
+
+            actual_end += block_len;
+        }
+    }
+}
 
 fn main() {
     println!("Hello, world!");
 
     let text = "aaabababababababbbabbabbabbaba";
-
-
 }
